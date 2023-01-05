052772f72546c1382246271f57171c83b70b6928
Romel Carino <carinorome@gmail.com>
2023-01-05 12:43:53 +0800

    20230105

---
+++ scripts/getDates.js
@@ -0,0 +1,3 @@
+document.getElementById('lastModified').innerHTML = `Last Modified ${document.lastModified}`;
+const date = new Date();
+document.getElementById('copyWrite').innerHTML = `&copy ${date.getFullYear()} Romelito Carino, Philippines, Pangasinan`;
