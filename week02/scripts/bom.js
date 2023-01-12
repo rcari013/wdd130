e2ebf9e92ab21bacb6a2155512e547e2f188741b
Romel Carino <carinorome@gmail.com>
2023-01-12 21:39:54 +0800

    20230112

---
+++ week02/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != ''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+    } else {
+        input.focus()
+        alert('The input field is empty :(');
+    };
+    
+});
+
+
+
+input.focus();
+input.value = '';
