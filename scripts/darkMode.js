efaf4bb970adc43df4f78b218974e92557673617
Romel Carino <carinorome@gmail.com>
2023-01-12 23:59:16 +0800

    2359

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -3,21 +3,24 @@ let light_mode = true;
 const bodyElement = document.querySelector('body');
 const cards = document.querySelectorAll('.card');
 const darkText = document.querySelectorAll('a, p, h2');
-const menuItems = document.querySelectorAll('.menuListItem')
+const menuItems = document.querySelectorAll('.menu-list-item')
+const menu_item = document.querySelectorAll('.menu-item')
+
 
 
 darkModeButton.addEventListener("click", () => {
 	if (light_mode) {
 		menuItems.forEach((item) => item.classList.toggle('darkMenu'));
 		bodyElement.classList.toggle('darkBody');
-        darkText.forEach((item) => item.classList.toggle("darkChar"));
+		darkText.forEach((item) => item.classList.toggle("darkChar"));
         cards.forEach((item) => item.classList.toggle("darkCard"));
 		light_mode = false;
+
 	} else if (!light_mode) {
 		menuItems.forEach((item) => item.classList.toggle('darkMenu'));
 		bodyElement.classList.toggle('darkBody');
-        darkText.forEach((item) => item.classList.toggle("darkChar"));
+		darkText.forEach((item) => item.classList.toggle("darkChar"));
         cards.forEach((item) => item.classList.toggle("darkCard"));
-        light_mode = true;
+		light_mode = true;
 	}
 });
