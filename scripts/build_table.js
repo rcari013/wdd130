f255a0eebf44262cc3f61b4b83054705569bea15
Romel Carino <carinorome@gmail.com>
2023-01-27 23:16:05 +0800

    1115pm

---
+++ scripts/build_table.js
@@ -0,0 +1,8 @@
+function build_table(){
+	///const table_of_form = document.createElement("table")
+	///document.getElementById("form_submitted").appendChild(table_of_form)
+	const text_1 = document.getElementsByName('fname').value;
+	document.getElementById('spanResult').textContent = text_1;
+}
+
+
